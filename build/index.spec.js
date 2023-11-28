7643ee006d4cfce222276cc6592573030a115ea0
Banks Nussman <banks@nussman.us>
2023-11-27 20:43:51 -0500

    clean up

--- build/index.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const test_1 = require("@playwright/test");
-(0, test_1.test)('Toyota', async ({ page }) => {
-    await page.goto('https://www.toyota.com/search-inventory/model/tacoma/?zipcode=28270');
-    page.on('response', (response) => {
-        console.log('<<', response.status(), response.url());
-    });
-});
